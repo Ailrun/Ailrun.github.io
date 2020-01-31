88e333be84e2673842c0b4ff4a5138b3ea67001c
Junyoung Clare Jang <jjc9310@gmail.com>
2020-01-31 02:37:43 -0500

    Port Halogen projects page component to React

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,20 +1,19 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React, { useState } from 'react';
 
-import HalogenLoader from '../components/HalogenLoader';
 import Layout from '../components/Layout';
 import NavigationBar from '../components/NavigationBar';
-import { component as ProjectsPageComponent } from '../purescript/Component/ProjectsPage.purs';
 
 const ProjectsPage: React.FC<unknown> = () => {
-  const [contentRef, setContentRef] = useState<HTMLDivElement>(null);
   const data = useStaticQuery<any>(query);
 
   return (
     <Layout>
-      <div ref={setContentRef}></div>
       <NavigationBar />
-      <HalogenLoader component={ProjectsPageComponent} input={data.json.projectSections} target={contentRef} />
+      <section className='ailrun-blog-projects-page'>
+        <Title />
+        <ProjectSections projectSections={data.json.projectSections} />
+      </section>
     </Layout>
   );
 };
@@ -34,3 +33,65 @@ const query = graphql`
     }
   }
 `;
+
+const Title: React.FC<unknown> = () => {
+  return (
+    <section className='ailrun-blog-page-title'>
+      <img src='https://raw.githubusercontent.com/Ailrun/media/master/blog-img/project.png' />
+      <header>
+        <h1>Projects</h1>
+      </header>
+    </section>
+  );
+};
+
+const ProjectSections: React.FC<any> = ({ projectSections }) => {
+  return (
+    <section className='ailrun-blog-page-main'>
+      {
+        projectSections.map((projectSection: any) => (
+          <ProjectSection {...{ projectSection }} />
+        ))
+      }
+    </section>
+  );
+};
+
+const ProjectSection: React.FC<any> = ({ projectSection }) => {
+  return (
+    <div>
+      <h2>{projectSection.title}</h2>
+      <hr />
+      <ul>
+      {
+        projectSection.projects.map((project: any, i: number) => (
+          <>
+            { i !== 0 ? <hr /> : null }
+            <Project {...{ project }} />
+          </>
+        ))
+      }
+      </ul>
+    </div>
+  );
+};
+
+const Project: React.FC<any> = ({ project }) => {
+  return (
+    <li>
+      <article>
+        <p>
+          <span>-</span>
+          <a href={project.link}>{project.title}</a>
+        </p>
+        <div>
+          {
+            project.images.map((image: string) => (
+              <img src={image} />
+            ))
+          }
+        </div>
+      </article>
+    </li>
+  );
+};
