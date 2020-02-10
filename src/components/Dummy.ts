1ef8bd759dc61fc208cf90e7b22b713828655fb7
Junyoung Clare Jang <jjc9310@gmail.com>
2020-02-10 00:15:27 -0500

    Extract dummy as an independent util component

---
+++ src/components/Dummy.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Dummy: React.FC = () => null;
+export default Dummy;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import Dummy from '../components/Dummy';
 
 /**
  * This `Dummy` component exists only because
  * Gatsby does not allow one to redirect index page to other page
  * without index file.
  */
-const Dummy: React.FC<unknown> = () => null;
 export default Dummy;
