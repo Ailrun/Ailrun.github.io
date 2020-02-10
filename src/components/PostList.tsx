bfac029f836371f8d74044250ffcbae98545c4f8
Junyoung Clare Jang <jjc9310@gmail.com>
2020-02-10 02:38:00 -0500

    Extract post list as a component

---
+++ src/components/PostList.tsx
@@ -0,0 +1,88 @@
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+import React, { Fragment } from 'react';
+
+import * as C from '../styles/constants';
+
+import FlexSpacer from './FlexSpacer';
+
+export interface Props {
+  readonly posts: PostInfo[];
+}
+const PostList: React.FC<Props> = ({ posts }) => (
+  <PostListWrapper>
+    {
+      posts.map((post) => (
+        <Fragment key={post.id}>
+          <Post {...{ post }} />
+        </Fragment>
+      ))
+    }
+  </PostListWrapper>
+);
+export default PostList;
+
+export interface PostInfo {
+  readonly id: string;
+  readonly title: string;
+  readonly date: string;
+  readonly excerpt: string;
+  readonly postPath: string;
+}
+
+const PostListWrapper = styled.ul({
+  width: '100%',
+
+  listStyle: 'none',
+});
+
+interface PostProps {
+  post: PostInfo;
+}
+const Post: React.FC<PostProps> = ({ post }) => (
+  <PostWrapper>
+    <PostLink to={post.postPath}>
+      <PostTitle>{post.title}</PostTitle>
+      <FlexSpacer />
+      <PostDate>{post.date}</PostDate>
+      <PostExcerpt>{post.excerpt} ...</PostExcerpt>
+    </PostLink>
+  </PostWrapper>
+);
+
+const PostWrapper = styled.li({
+  width: '100%',
+
+  '& + &': {
+    marginTop: '2vw',
+  },
+});
+
+const PostLink = styled(Link)({
+  display: 'flex',
+
+  width: '100%',
+
+  alignItems: 'center',
+  flexWrap: 'wrap',
+});
+
+const PostTitle = styled.h3({
+  fontSize: C.fontLargeSize,
+  color: C.textBlack,
+});
+
+const PostDate = styled.span({
+  fontSize: C.fontSmallSize,
+  color: C.textVeryLightBlack,
+});
+
+const PostExcerpt = styled.span({
+  marginTop: '0.5vw',
+  marginLeft: '2%',
+
+  width: '98%',
+
+  fontSize: C.fontBaseSize,
+  color: C.textLightBlack,
+});
--- src/templates/PostsTemplate.tsx
+++ src/templates/PostsTemplate.tsx
@@ -1,54 +1,48 @@
 import styled from '@emotion/styled';
-import { Link, PageRendererProps, graphql } from 'gatsby';
-import React, { Fragment } from 'react';
+import { PageRendererProps, graphql } from 'gatsby';
+import React from 'react';
 
-import FlexSpacer from '../components/FlexSpacer';
 import Layout from '../components/Layout';
 import NavigationBar from '../components/NavigationBar';
 import PageTitle from '../components/PageTitle';
+import PostList, { PostInfo as PostListPostInfo } from '../components/PostList';
 import { locationToLanguage } from '../languages';
-import * as C from '../styles/constants';
 
 interface Props extends PageRendererProps {
   readonly data: Data;
 }
-const PostsTemplate: React.FC<Props> = ({ data, location }) => (
-  <Layout>
-    <NavigationBar language={locationToLanguage(location)} />
-    <PageTitle
-      backgroundSrc='https://raw.githubusercontent.com/Ailrun/media/master/blog-img/post.png'
-      title='Posts'
-    />
-    <PostListWrapper>
-      <PostList>
-        {
-          data.md.posts.map((post) => (
-            <Fragment key={post.id}>
-              <Post {...{ post }} />
-            </Fragment>
-          ))
-        }
-      </PostList>
-    </PostListWrapper>
-  </Layout>
-);
+const PostsTemplate: React.FC<Props> = ({ data, location }) => {
+  const posts = refineData(data);
+
+  return (
+    <Layout>
+      <NavigationBar language={locationToLanguage(location)} />
+      <PageTitle
+        backgroundSrc='https://raw.githubusercontent.com/Ailrun/media/master/blog-img/post.png'
+        title='Posts'
+      />
+      <PostListWrapper>
+        <PostList posts={posts} />
+      </PostListWrapper>
+    </Layout>
+  );
+};
 export default PostsTemplate;
 
 interface Data {
   readonly md: {
-    readonly posts: Post[];
-  };
-}
-interface Post {
-  readonly frontmatter: {
-    readonly title: string;
-    readonly date?: string;
-  };
-  readonly id: string;
-  readonly excerpt: string;
-  readonly postPath: string;
-  readonly parent: {
-    readonly date: string;
+    readonly posts: {
+      readonly frontmatter: {
+        readonly title: string;
+        readonly date?: string;
+      };
+      readonly id: string;
+      readonly excerpt: string;
+      readonly postPath: string;
+      readonly parent: {
+        readonly date: string;
+      };
+    }[];
   };
 }
 export const query = graphql`
@@ -73,66 +67,19 @@ export const query = graphql`
   }
 `;
 
+const refineData = (data: Data): PostListPostInfo[] => {
+  return data.md.posts.map((post) => ({
+    id: post.id,
+    title: post.frontmatter.title,
+    date: post.frontmatter.date ?? post.parent.date,
+    excerpt: post.excerpt,
+    postPath: post.postPath,
+  }));
+};
+
 const PostListWrapper = styled.main({
   margin: '0 20%',
   marginTop: '2vw',
 
   width: '60%',
-});
-
-const PostList = styled.ul({
-  width: '100%',
-
-  listStyle: 'none',
-});
-
-interface PostProps {
-  post: Post;
-}
-const Post: React.FC<PostProps> = ({ post }) => (
-  <PostWrapper>
-    <PostLink to={post.postPath}>
-      <PostTitle>{post.frontmatter.title}</PostTitle>
-      <FlexSpacer />
-      <PostDate>{post.frontmatter.date ?? post.parent.date}</PostDate>
-      <PostExcerpt>{post.excerpt} ...</PostExcerpt>
-    </PostLink>
-  </PostWrapper>
-);
-
-const PostWrapper = styled.li({
-  width: '100%',
-
-  '& + &': {
-    marginTop: '2vw',
-  },
-});
-
-const PostLink = styled(Link)({
-  display: 'flex',
-
-  width: '100%',
-
-  alignItems: 'center',
-  flexWrap: 'wrap',
-});
-
-const PostTitle = styled.h3({
-  fontSize: C.fontLargeSize,
-  color: C.textBlack,
-});
-
-const PostDate = styled.span({
-  fontSize: C.fontSmallSize,
-  color: C.textVeryLightBlack,
-});
-
-const PostExcerpt = styled.span({
-  marginTop: '0.5vw',
-  marginLeft: '2%',
-
-  width: '98%',
-
-  fontSize: C.fontBaseSize,
-  color: C.textLightBlack,
 });
