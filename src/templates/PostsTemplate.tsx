88f4e40ba24cddda4b72ad260d59b60700d007ad
Junyoung Clare Jang <jjc9310@gmail.com>
2020-02-21 23:59:30 -0500

    Sort posts in descending order of the posting date

--- src/templates/PostsTemplate.tsx
+++ src/templates/PostsTemplate.tsx
@@ -35,12 +35,14 @@ interface Data {
       readonly frontmatter: {
         readonly title: string;
         readonly date?: string;
+        readonly dateForSort?: string;
       };
       readonly id: string;
       readonly excerpt: string;
       readonly postPath: string;
       readonly parent: {
         readonly date: string;
+        readonly dateForSort: string;
       };
     }[];
   };
@@ -52,6 +54,7 @@ export const query = graphql`
         frontmatter {
           title
           date(fromNow: true)
+          dateForSort: date
         }
         id
         excerpt
@@ -60,6 +63,7 @@ export const query = graphql`
         parent {
           ... on File {
             date: birthTime(fromNow: true)
+            dateForSort: birthTime
           }
         }
       }
@@ -68,13 +72,23 @@ export const query = graphql`
 `;
 
 const refineData = (data: Data): PostListPostInfo[] => {
-  return data.md.posts.map((post) => ({
-    id: post.id,
-    title: post.frontmatter.title,
-    date: post.frontmatter.date ?? post.parent.date,
-    excerpt: post.excerpt,
-    postPath: post.postPath,
-  }));
+  return data.md.posts
+    .map((post) => ({
+      id: post.id,
+      title: post.frontmatter.title,
+      date: post.frontmatter.date ?? post.parent.date,
+      dateForSort: post.frontmatter.dateForSort ?? post.parent.dateForSort,
+      excerpt: post.excerpt,
+      postPath: post.postPath,
+    }))
+    .sort((post0, post1) => Date.parse(post1.dateForSort) - Date.parse(post0.dateForSort))
+    .map((post) => ({
+      id: post.id,
+      title: post.title,
+      date: post.date,
+      excerpt: post.excerpt,
+      postPath: post.postPath,
+    }));
 };
 
 const PostListWrapper = styled.main({
