9418e34407c0373c9b3ae549b74122414a126d92
Junyoung Clare Jang <jjc9310@gmail.com>
2020-02-27 15:02:42 +0000

    Update from Gatsby

--- component---src-pages-ko-index-tsx-2ccc550aee68dd21df26.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{boto:function(t,o,n){"use strict";var c=n("Wbzz"),e=n("q1tI"),a=n("/Hoz");o.a=function(t){var o=t.location;return Object(e.useEffect)((function(){var t=Object(a.b)(o);Object(c.b)("/"+t+"/posts/",{replace:!0})}),[o]),null}},lD38:function(t,o,n){"use strict";n.r(o);var c=n("boto");o.default=c.a}}]);
-//# sourceMappingURL=component---src-pages-ko-index-tsx-2ccc550aee68dd21df26.js.map
