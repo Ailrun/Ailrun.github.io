83333cec28d27fc91967007955bdaf0d7b516138
Junyoung Clare Jang <jjc9310@gmail.com>
2020-02-28 21:02:54 -0500

    Remove unused vietnamese css rules

--- gatsby/wrapRootElement.tsx
+++ gatsby/wrapRootElement.tsx
@@ -13,7 +13,25 @@ const wrapRootElement = ({ element }: WrapRootElementArgs): React.ReactChild => 
 export default wrapRootElement;
 
 const globalStyles = css`
-@import url('https://fonts.googleapis.com/css?family=Dancing+Script:400,700|Noto+Serif+KR:400,700&display=block&subset=korean');
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=block');
+
+@font-face {
+  font-family: 'Dancing Script';
+  font-style: normal;
+  font-weight: 400;
+  font-display: block;
+  src: url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+@font-face {
+  font-family: 'Dancing Script';
+  font-style: normal;
+  font-weight: 700;
+  font-display: block;
+  src: url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 
 * {
   margin: 0;
