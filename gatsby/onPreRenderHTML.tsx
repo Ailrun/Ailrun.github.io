fd2e3cc9ec632fc276f7060fe9db9b9e5a855896
Junyoung Clare Jang <jjc9310@gmail.com>
2020-03-04 12:39:16 -0500

    Fix an issue of onPreRenderHTML in dev mode

--- gatsby/onPreRenderHTML.tsx
+++ gatsby/onPreRenderHTML.tsx
@@ -1,28 +1,17 @@
 import { PreRenderHTMLArgs } from 'gatsby';
-import React, { ReactElement } from 'react';
+import React from 'react';
 
 const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }: PreRenderHTMLArgs): void => {
-  const newHeadComponents = getHeadComponents()
-    .map((component) => {
-      if ((component as ReactElement | undefined | null)?.type === 'link'
-          && (component as ReactElement).props?.rel === 'preload'
-          && !((component as ReactElement).props.href as string).startsWith('/')) {
-        (component as ReactElement).props.crossOrigin = 'anonymous';
-      }
-
-      return component;
-    })
-    .concat([
-      (
-        <link
-          rel='preconnect dns-prefetch'
-          href='https://c.disquscdn.com'
-          crossOrigin='anonymous'
-          key='https://c.disquscdn.com'
-        />
-      ),
-    ]);
-
-  replaceHeadComponents(newHeadComponents);
+  replaceHeadComponents([
+    ...getHeadComponents(),
+    (
+      <link
+        rel='preconnect dns-prefetch'
+        href='https://c.disquscdn.com'
+        crossOrigin='anonymous'
+        key='https://c.disquscdn.com'
+      />
+    ),
+  ]);
 };
 export default onPreRenderHTML;
