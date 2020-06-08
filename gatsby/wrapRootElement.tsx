443666bc4400b418ae0afd9af58895b4ccbe8d73
Junyoung Clare Jang <jjc9310@gmail.com>
2020-06-08 15:51:20 -0400

    Update fonts

--- gatsby/wrapRootElement.tsx
+++ gatsby/wrapRootElement.tsx
@@ -25,7 +25,7 @@ const globalStyles = css`
   font-style: normal;
   font-weight: 400;
   font-display: block;
-  src: url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
+  src: local(Dancing Script Regular), local(DancingScript-Regular), local(Dancing Script), local(DancingScript), url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 
@@ -34,7 +34,7 @@ const globalStyles = css`
   font-style: normal;
   font-weight: 700;
   font-display: block;
-  src: url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
+  src: local(Dancing Script Bold), local(DancingScript-Bold), url(https://fonts.gstatic.com/s/dancingscript/v14/If2RXTr6YS-zF4S-kcSWSVi_szLgiuE.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 
