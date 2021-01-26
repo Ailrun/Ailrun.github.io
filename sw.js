579c4476202ba81af3af6d3e2cffd4637a4e6b49
Junyoung Clare Jang <jjc9310@gmail.com>
2021-01-26 16:05:06 +0000

    Update from Gatsby

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5d7527ca48385b36abb3.js"
+    "url": "webpack-runtime-3b11d103f2540f889697.js"
   },
   {
     "url": "framework-1a1efb3f5c798a012f4a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "280247da11f8e8ccc68b17f8f642f86a"
+    "revision": "33f334c3a8477bdaf44507a7bf2f9ae3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c0a6e264e612c6dfdf34ef67c468dc94"
+    "revision": "fcb59f84d3200399928326bfabe6781f"
   },
   {
     "url": "component---src-pages-ko-index-tsx-7cf2f8ad98e2381ecbae.js"
@@ -67,7 +67,7 @@ self.__precacheManifest = [
     "revision": "a87a9aeb3a157962c351adbe34cae151"
   },
   {
-    "url": "component---src-pages-ko-posts-ts-8b1ce7d3ac19cab4c24a.js"
+    "url": "component---src-pages-ko-posts-ts-c8c77861161bd89c049a.js"
   },
   {
     "url": "page-data/ko/posts/page-data.json",
